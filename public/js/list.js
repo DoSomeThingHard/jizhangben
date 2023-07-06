be4b50c20cb1bcf0d3d73bba93b20218e26abb8a
HQ01UH357 <13664283060@163.com>
2023-07-06 13:52:04 +0800

    小修改

--- public/js/list.js
+++ public/js/list.js
@@ -4,7 +4,7 @@ window.onload = function () {
         item.onclick = function (e) {
             // 返回 true就是可以响应默认行为  返回false就是不响应默认行为
             if (confirm(`您确定要删除${item.dataset.title}吗?`)) {
-                return true;    // 返回true 响应默认的事件
+                return true;     // 返回true 响应默认的事件
             } else {
                 e.preventDefault();
             }
